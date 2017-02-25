1f4f9a481eb39900cf00a63300ec48c2323693db
Harshal Kapure <harshal.kapure.consultant@nielsen.com>
2017-02-26 00:04:31 +0530

    added loader configs

---
+++ LoaderConfigs/Loaderconfig.js
@@ -0,0 +1,11 @@
+Ext.Loader.setConfig({
+    enabled: true,
+    //disableCaching : true,  
+    paths: {
+        'm1': '../m1',
+        'm2': '../m2',
+
+    }
+});
+//access objects in anoter war using m1 as object
+//m1.resources
